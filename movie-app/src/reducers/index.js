b74a5f89a81f747240e66ab26c02a04e2da911f6
Ekansh Gupta <ekanshgupta2019@gmail.com>
2023-03-30 19:34:13 +0530

    Added unfavourite option

--- movie-app/src/reducers/index.js
+++ movie-app/src/reducers/index.js
@@ -1,16 +1,50 @@
 import { ADD_MOVIES } from "../actions";
-
+import { ADD_FAVOURITE } from "../actions";
+import { REMOVE_FROM_FAVOURITES } from "../actions";
+import { SET_SHOW_FAVOURITES } from "../actions";
 
 const initialMoviesState = {
     list: [],
-    favourites: []
+    favourites: [],
+    showFavourites: false,
 }
 export default function movies (state = initialMoviesState, action) {
-    if(action.type === ADD_MOVIES){
-        return {
-            ...state,
-            list: action.movies
-        }
+    switch (action.type) {
+        case ADD_MOVIES: 
+            return {
+                ...state , 
+                list: action.movies
+            }
+        case ADD_FAVOURITE: 
+            return {
+                ...state,
+                favourites: [action.movie, ...state.favourites]
+            }
+        case REMOVE_FROM_FAVOURITES: 
+            const filteredArray = state.favourites.filter(
+                movie => movie.Title !== action.movie.Title
+            );
+
+            return {
+                ...state,
+                favourites : filteredArray
+            }
+        case SET_SHOW_FAVOURITES:
+            return {
+                ...state,
+                showFavourites: action.val
+            }
+        default: 
+            return state;
     }
-    return state;
+
+
+
+    // if(action.type === ADD_MOVIES){
+    //     return {
+    //         ...state,
+    //         list: action.movies
+    //     }
+    // }
+    // return state;
 }
