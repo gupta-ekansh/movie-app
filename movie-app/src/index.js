d0ac6883d07ea5afd3a948ed247b251460ccd4d6
Ekansh Gupta <ekanshgupta2019@gmail.com>
2023-03-30 15:28:28 +0530

    subsrcibed to the store

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import {configureStore} from '@reduxjs/toolkit';
+// import {createStore} from 'redux'
+import movies from './reducers';
+
+const store = configureStore({reducer: movies})
+console.log(store);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>
 );
-  
+
